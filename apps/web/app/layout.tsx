1805fa1f55e371c8e588086ef44abdb60185c667
yongho1212 <yongho6800@gmail.com>
2024-09-30 04:13:45 +0000

    f/doc/metadata/yong

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,15 +2,13 @@ import './globals.css'
 import type {Metadata} from 'next'
 import { Suspense } from 'react'
 import Loading from './components/Loading'
-import {ClientAuthProvider} from './providers/AuthProvider'
 import NavBar from './components/NavBar';
 import HeaderBar from './components/HeaderBar';
 
 
-
 export const metadata: Metadata = {
-    title: '활동메이트 찾기 앱',
-    description: '다양한 활동을 함께할 메이트를 찾아보세요!',
+    title: 'Cellabor',
+    description: '원하는 스타일의 촬영메이트를 쉽게 찾아보세요',
 }
 
 export default function RootLayout({
